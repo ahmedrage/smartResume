8847ce7c3e58d0ee4d4c4be29e68dcf84d04a948
Ahmed Azhar <ahmed@hardsoftware.com>
2024-07-31 02:10:51 +0930

    Added docx support

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, HTTPException, Request, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from app.preprocess import PreProcessor
 from app.model import ResumeCNNModel
-from pdfminer.high_level import extract_text
+from pdfminer.high_level import extract_text as extract_text_from_pdf
+from docx import Document
 import json
 import tempfile
 import os
@@ -11,8 +12,8 @@ app = FastAPI()
 
 # Set up CORS
 origins = [
-    "http://localhost:3000",  # Your React frontend running locally
-    "https://your-production-domain.com",  # Add your production frontend domain when deploying
+    "http://localhost:3000",
+    "https://smart-resume.meteorapp.com",
 ]
 
 app.add_middleware(
@@ -60,21 +61,30 @@ async def predict(request: Request):
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/extract-text")
-async def extract_text_from_pdf(file: UploadFile = File(...)):
+async def extract_text_from_file(file: UploadFile = File(...)):
     try:
-        # Write the uploaded PDF file to a temporary file
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+        # Determine the file extension
+        file_ext = os.path.splitext(file.filename)[1].lower()
+
+        # Write the uploaded file to a temporary file
+        with tempfile.NamedTemporaryFile(delete=False, suffix=file_ext) as temp_file:
             temp_file.write(await file.read())
             temp_file_path = temp_file.name
 
-        # Extract text from the PDF
-        text = extract_text(temp_file_path)
+        # Extract text based on file type
+        if file_ext == ".pdf":
+            text = extract_text_from_pdf(temp_file_path)
+        elif file_ext in [".doc", ".docx"]:
+            doc = Document(temp_file_path)
+            text = "\n".join([para.text for para in doc.paragraphs])
+        else:
+            raise HTTPException(status_code=400, detail="Unsupported file type")
 
         # Remove the temporary file
         os.remove(temp_file_path)
 
         if not text:
-            raise HTTPException(status_code=400, detail="No text extracted from PDF")
+            raise HTTPException(status_code=400, detail="No text extracted from file")
 
         return {"text": text}
     except Exception as e:
